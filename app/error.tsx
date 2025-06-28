faae8cbbf7ce4a9208a47f5f8f970e8f5ccf4a84
Notjessand <jessennorom@gmail.com>
2025-06-28 18:32:46 +0100

    x

--- app/error.tsx
+++ app/error.tsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 
 const Error = () => {
   return (
-    <div className="h-screen w-screen flex justify-center items-center font-montserrat">
+    <div className="h-screen w-screen flex flex-col justify-center items-center font-montserrat">
       <p>We&apos;ve encountered a problem</p>
-      <Link href="/dashboard" className="flex mt-2 underline">
+      <Link
+        href="/dashboard"
+        className="flex px-4 py-2 bg-slate-300 rounded-lg mt-3"
+      >
         Go to Dashboard
       </Link>
     </div>
